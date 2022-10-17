0483f1d30e27630231a0f426dd6d5db5ed0f7998
Grammy Jiang <grammy.jiang@gmail.com>
2022-10-18 00:04:18 +1100

    Add the empty application of aria2

---
+++ aria2/apps.py
@@ -0,0 +1,15 @@
+"""
+Configure the application of aria2
+
+* https://docs.djangoproject.com/en/4.1/ref/applications/
+"""
+from django.apps import AppConfig
+
+
+class Aria2Config(AppConfig):
+    """
+    the configuration of the application of aria2
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "aria2"
--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -2,5 +2,6 @@
 The settings module to import by categories
 """
 from .api import *
+from .aria2 import *
 from .django import *
 from .third_parties import *
