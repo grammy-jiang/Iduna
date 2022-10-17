b5c930bdfa010f5f49eeb394b7a1e9e764cbc19a
Grammy Jiang <grammy.jiang@gmail.com>
2022-10-17 22:07:20 +1100

    Simplify rest folder of api

--- api/rest/urls.py
+++ api/rest/urls.py
@@ -1,7 +1,13 @@
+"""
+REST framework adds support for automatic URL routing to Django, and provides you with a
+simple, quick and consistent way of wiring your view logic to a set of URLs.
+
+* https://www.django-rest-framework.org/api-guide/routers/
+"""
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import UserViewSet
+from .user import UserViewSet
 
 router = routers.DefaultRouter()
 router.register("users", viewset=UserViewSet, basename="user")
--- api/rest/user.py
+++ api/rest/user.py
@@ -1,8 +1,9 @@
 """
-the serializer of User model
+the serializer and viewset of User model
 """
 from django.contrib.auth import get_user_model
 from rest_framework.serializers import ModelSerializer
+from rest_framework.viewsets import ModelViewSet
 
 User = get_user_model()
 
@@ -15,3 +16,12 @@ class UserSerializer(ModelSerializer):
     class Meta:
         fields = "__all__"
         model = User
+
+
+class UserViewSet(ModelViewSet):  # pylint: disable=too-many-ancestors
+    """
+    the viewset of User model
+    """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
