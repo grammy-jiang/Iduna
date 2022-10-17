2baa74746a7d0b1fbc77db3daf5be0ef33ccd8da
Grammy Jiang <grammy.jiang@gmail.com>
2022-10-17 21:40:47 +1100

    Add comments

--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,15 @@
+"""
+Configure the application of api
+
+* https://docs.djangoproject.com/en/4.1/ref/applications/
+"""
 from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
+    """
+    the configuration of the application of api
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "api"
--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,3 +1,6 @@
+"""
+The settings module to import by categories
+"""
 from .api import *
 from .django import *
 from .third_parties import *
