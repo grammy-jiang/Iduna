4312e3ec3e11049de08c53111f3e876223ce7868
Grammy Jiang <grammy.jiang@gmail.com>
2022-10-18 20:33:24 +1100

    Add version to display in Aria2c admin of aria2

--- aria2/admin/aria2c.py
+++ aria2/admin/aria2c.py
@@ -1,6 +1,8 @@
 """
 The admin of Aria2 model of aria2
 """
+import subprocess
+
 from django.contrib import admin
 
 from aria2.models import Aria2c
@@ -11,3 +13,18 @@ class Aria2Admin(admin.ModelAdmin):
     """
     The admin of Aria2 model of aria2
     """
+
+    fields = ("path", "version")
+    list_display = ("path",)
+    readonly_fields = ("version",)
+
+    @admin.display()
+    def version(self, obj: Aria2c) -> str:
+        """
+
+        :param obj:
+        :type obj: Aria2c
+        :return:
+        :rtype: str
+        """
+        return subprocess.check_output([obj.path, "--version"]).decode()
