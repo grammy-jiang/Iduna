3a5d7d5c8d6c0138f6d058a3af9523a55d92250c
Grammy Jiang <grammy.jiang@gmail.com>
2022-10-20 23:18:49 +1100

    Add Aria2cInstance model to aria2

--- aria2/models/__init__.py
+++ aria2/models/__init__.py
@@ -2,3 +2,4 @@
 The models of the application of aria2
 """
 from .aria2c import Aria2c
+from .aria2c_instance import Aria2cInstance
---
+++ aria2/models/aria2c_instance.py
@@ -0,0 +1,49 @@
+"""
+The model of Aria2 Instance
+"""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from django.db import models
+
+if TYPE_CHECKING:
+    from .aria2c import Aria2c
+
+
+class QuerySet(models.QuerySet):
+    """
+    custom QuerySet to fit aria2c
+    """
+
+    def create_all_from_aria2c(self, aria2c: Aria2c) -> tuple[Aria2cInstance, ...]:
+        """
+
+        :param aria2c:
+        :type aria2c: Aria2c
+        :return:
+        :rtype: tuple[Aria2cInstance, ...]
+        """
+        return tuple(
+            self.create(pid=pid, command=aria2c._get_command(pid), aria2c=aria2c)
+            for pid in aria2c._get_pids()
+        )
+
+
+class Manager(models.Manager):
+    """
+    custom Manager to fit aria2c
+    """
+
+
+class Aria2cInstance(models.Model):
+    """
+    The model of Aria2 instance
+    """
+
+    pid = models.IntegerField()
+    command = models.CharField(max_length=256)
+
+    aria2c = models.ForeignKey("Aria2c", on_delete=models.CASCADE)
+
+    objects = Manager.from_queryset(QuerySet)()
