3fc42be13b3f8d517b94b4e5057491bb324f03cf
Grammy Jiang <grammy.jiang@gmail.com>
2022-10-24 01:27:45 +1100

    Update admins of aria2

--- aria2/admin/aria2c_profile.py
+++ aria2/admin/aria2c_profile.py
@@ -14,6 +14,7 @@ class ArgumentPairAdmin(admin.ModelAdmin):
     """
 
     list_display = ("profile", "argument", "value")
+    ordering = ("profile", "argument")
 
 
 class ArgumentPairInline(admin.TabularInline):
