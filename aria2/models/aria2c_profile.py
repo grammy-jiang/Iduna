c4eada3a65fbc3d8ea971f41125bcae851025c46
Grammy Jiang <grammy.jiang@gmail.com>
2022-10-25 21:34:05 +1100

    Update Aria2Profile model of aria2

--- aria2/models/aria2c_profile.py
+++ aria2/models/aria2c_profile.py
@@ -3,6 +3,8 @@ The model of Aria2 Profile
 """
 from __future__ import annotations
 
+from typing import Iterable, Optional
+
 from django.db import models
 from django.utils.functional import cached_property
 
@@ -17,6 +19,8 @@ class Aria2cProfile(models.Model):
 
     arguments = models.ManyToManyField("Aria2cArgument", through="ArgumentPair")
 
+    args = models.CharField(blank=True, max_length=256, null=True)
+
     class Meta:
         verbose_name = "Aria2c - Profile"
 
@@ -28,8 +32,31 @@ class Aria2cProfile(models.Model):
         """
         return self.name
 
+    def save(
+        self,
+        force_insert: bool = False,
+        force_update: bool = False,
+        using: Optional[str] = None,
+        update_fields: Optional[Iterable[str]] = None,
+    ) -> None:
+        """
+
+        :param force_insert:
+        :type force_insert: bool
+        :param force_update:
+        :type force_update: bool
+        :param using:
+        :type using: Optional[str]
+        :param update_fields:
+        :type update_fields: Optional[Iterable[str]]
+        :return:
+        :rtype: None
+        """
+        self.args = " ".join(self._args)
+        return super().save(force_insert, force_update, using, update_fields)
+
     @cached_property
-    def args(self) -> tuple[str, ...]:
+    def _args(self) -> tuple[str, ...]:
         """
 
         :return:
@@ -45,7 +72,7 @@ class Aria2cProfile(models.Model):
         :return:
         :rtype: tuple[str, ...]
         """
-        return str(self.aria2c.path), *self.args
+        return str(self.aria2c.path), *self._args
 
 
 class ArgumentPair(models.Model):
