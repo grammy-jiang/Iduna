e35e76df7a1f9216a61301177aed5e790e88144a
Grammy Jiang <grammy.jiang@gmail.com>
2022-10-27 16:42:43 +1100

    Update TimeStampMixin model of aria2

--- aria2/models/utils.py
+++ aria2/models/utils.py
@@ -9,8 +9,8 @@ class TimeStampMixin(models.Model):
     a mixin to record created and updated timestamp
     """
 
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
