ec5af0d34f081d7848c11fddc4f0f384044733ce
Grammy Jiang <grammy.jiang@gmail.com>
2022-11-25 08:56:37 +1100

    Rename aria2c_argument to argument of aria2

--- aria2/admin/__init__.py
+++ aria2/admin/__init__.py
@@ -1,7 +1,7 @@
 """
 The admins of the models of the application of aria2
 """
-from .aria2c_argument import *
+from .argument import *
 from .aria2c_gid import *
 from .aria2c_instance import *
 from .aria2c_profile import *
